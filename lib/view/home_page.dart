1f84347efd88dd711f8dda00223a1423dec43d47
MuhammedFaasil <muhammedfaasilpty@gmail.com>
2024-03-01 10:42:52 +0530

    home page added

---
+++ lib/view/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ResumeHomePage extends StatelessWidget {
+  const ResumeHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Build Your Resume'),),
+    );
+  }
+}
